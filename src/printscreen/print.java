aaf8e0858573bb7ae1a4a02e7a9a89c60d17c8bb
jamab <abjohn555@gmail.com>
2016-06-06 17:43:38 +0500

    msg1

---
+++ src/printscreen/print.java
@@ -0,0 +1,9 @@
+package printscreen;
+
+public class print {
+	public static void main(String args[]){
+		System.out.println("Enter two integers to calculate their sum ");
+		System.out.println("Enter two integers to calculate their sum ");
+		 
+	}
+}
