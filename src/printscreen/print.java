5266b861728987529b8105b29ae83dddcb5e761e
jamab <abjohn555@gmail.com>
2016-06-06 17:45:56 +0500

    msg4

--- src/printscreen/print.java
+++ src/printscreen/print.java
@@ -3,7 +3,7 @@ package printscreen;
 public class print {
 	public static void main(String args[]){
 		System.out.println("Enter two integers to calculate their sum ");
-		System.out.println("Enter two integers to calculate their sum ");
+		
 		 
 	}
 }
